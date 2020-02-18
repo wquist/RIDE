25eccdd0bc7224db63b815b10f00f8814a2c4e24
Wyatt <git.wquist@gmail.com>
2020-02-18 21:32:05 +0000

    add some menus to their fragments

--- presentation/src/main/java/com/atlas/ride/presentation/feature/editor/EditorFragment.kt
+++ presentation/src/main/java/com/atlas/ride/presentation/feature/editor/EditorFragment.kt
@@ -1,15 +1,19 @@
 package com.atlas.ride.presentation.feature.editor
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 
 import com.atlas.ride.presentation.R
 
 class EditorFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, parent: ViewGroup?, state: Bundle?): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_editor, parent, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.editor, menu)
     }
 }
--- presentation/src/main/java/com/atlas/ride/presentation/feature/notifications/NotificationsFragment.kt
+++ presentation/src/main/java/com/atlas/ride/presentation/feature/notifications/NotificationsFragment.kt
@@ -1,15 +1,19 @@
 package com.atlas.ride.presentation.feature.notifications
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 
 import com.atlas.ride.presentation.R
 
 class NotificationsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, parent: ViewGroup?, state: Bundle?): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_notifications, parent, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.notifications, menu)
     }
 }
