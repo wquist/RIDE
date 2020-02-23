bad83a47aa481e406fb9354272de1eb8ce8ae18c
Wyatt <git.wquist@gmail.com>
2020-02-23 19:39:53 +0000

    separate icon selector into a compound control class

---
+++ presentation/src/main/java/com/atlas/ride/presentation/widget/IconSelector.kt
@@ -0,0 +1,17 @@
+package com.atlas.ride.presentation.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+import com.atlas.ride.presentation.R
+
+class IconSelector @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attrs, defStyle) {
+    init {
+        inflate(context, R.layout.content_icon, this)
+    }
+}
