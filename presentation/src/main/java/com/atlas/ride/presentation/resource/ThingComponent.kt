4e3bf67cb9680ac487e61124eb3ba85fa5d402e0
Wyatt <git.wquist@gmail.com>
2020-04-11 02:10:54 +0100

    document the thing component methods

--- presentation/src/main/java/com/atlas/ride/presentation/resource/ThingComponent.kt
+++ presentation/src/main/java/com/atlas/ride/presentation/resource/ThingComponent.kt
@@ -11,26 +11,35 @@ import kotlin.math.PI
 import kotlin.math.max
 import kotlin.math.sin
 
-class ThingComponent(val radius: Int, val minRadius: Int = 0) {
+/**
+ * A utility class that handles the actual drawing of a thing symbol (namely, the icon, circle
+ * background, and status in the corner). This allows for a common implementation amongst things,
+ * relationships, etc. The component only stores values that do not change across drawing multiple
+ * things (the radius and stroke color, in this case).
+ */
+class ThingComponent(var radius: Int, var minRadius: Int = 0) {
     private val strokePaint = Paint().apply {
         color = Color.WHITE
         flags = Paint.ANTI_ALIAS_FLAG
     }
 
+    /**
+     * Draw the thing symbol to the given canvas, using the stored radius.
+     */
     fun draw(canvas: Canvas, icon: Drawable, background: Paint, status: Paint) {
         val r = max(radius, minRadius).toFloat()
-        val s = r * 2f / icon.bounds.height()
-        val d = r * sin(45 * PI / 180).toFloat()
+        val iconSize = r * 2f / icon.bounds.height()
+        val statusOffset = r * sin(45 * PI / 180).toFloat()
 
         with(canvas) {
             drawCircle(r, r, r, background)
-            withScale(s * 0.6f, s * 0.6f) {
+            withScale(iconSize * 0.6f, iconSize * 0.6f) {
                 icon.draw(this)
             }
 
             withTranslation(r, r) {
-                drawCircle(d, -d, r * 0.30f, strokePaint)
-                drawCircle(d, -d, r * 0.25f, status)
+                drawCircle(statusOffset, -statusOffset, r * 0.30f, strokePaint)
+                drawCircle(statusOffset, -statusOffset, r * 0.25f, status)
             }
         }
     }
