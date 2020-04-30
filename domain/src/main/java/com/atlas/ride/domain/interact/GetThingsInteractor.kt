6e195558db55f29af2b551430da2c83ec1c3d9a9
Wyatt <git.wquist@gmail.com>
2020-05-01 00:38:08 +0100

    implement things view interactor

---
+++ domain/src/main/java/com/atlas/ride/domain/interact/GetThingsInteractor.kt
@@ -0,0 +1,22 @@
+package com.atlas.ride.domain.interact
+
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+
+import com.atlas.ride.domain.entity.IThing
+import com.atlas.ride.domain.repo.IThingsRepository
+import com.atlas.ride.domain.util.Lce
+
+class GetThingsInteractor(private val repo: IThingsRepository) : BaseInteractor<List<IThing>>() {
+    override suspend fun invoke(onChange: (Lce<List<IThing>>) -> Unit) {
+        onChange(Lce.Loading)
+        repo.get()
+            .catch { e ->
+                val msg = e.message ?: "GetThingsInteractor could not retrieve thing info."
+                onChange(Lce.Error(msg))
+            }
+            .collect {
+                onChange(Lce.Content(it))
+            }
+    }
+}
