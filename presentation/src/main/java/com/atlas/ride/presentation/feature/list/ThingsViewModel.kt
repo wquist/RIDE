d9c688d1607cda5cafa7c27e9dd8beb78490e093
Wyatt <git.wquist@gmail.com>
2020-08-30 05:07:42 +0100

    remove base class inheritance for list ViewModels

--- presentation/src/main/java/com/atlas/ride/presentation/feature/list/ThingsViewModel.kt
+++ presentation/src/main/java/com/atlas/ride/presentation/feature/list/ThingsViewModel.kt
@@ -1,13 +1,19 @@
 package com.atlas.ride.presentation.feature.list
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 
 import kotlinx.coroutines.launch
 
+import com.atlas.ride.domain.entity.IThing
 import com.atlas.ride.domain.interact.GetThingsInteractor
 import com.atlas.ride.domain.repo.IThingsRepository
+import com.atlas.ride.domain.util.Lce
+import com.atlas.ride.presentation.util.ViewState
 
-class ThingsViewModel(repo: IThingsRepository) : BaseViewModel() {
+class ThingsViewModel(repo: IThingsRepository) : ViewModel() {
+    val state = ViewState<Lce<List<IThing>>>(Lce.Loading)
+
     init {
         viewModelScope.launch {
             GetThingsInteractor(repo).invoke { state.value = it }
