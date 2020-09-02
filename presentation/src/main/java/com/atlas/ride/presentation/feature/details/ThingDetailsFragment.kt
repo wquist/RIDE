c74c38f3392a96d732697c35d059dcd54bdd484c
Wyatt <git.wquist@gmail.com>
2020-09-02 04:24:25 +0100

    define initial thing details layout and navigation

---
+++ presentation/src/main/java/com/atlas/ride/presentation/feature/details/ThingDetailsFragment.kt
@@ -0,0 +1,14 @@
+package com.atlas.ride.presentation.feature.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.atlas.ride.presentation.R
+
+class ThingDetailsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, parent: ViewGroup?, state: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_thingdetails, parent, false)
+    }
+}
